8ee06e29ddfbc2829253a48e395a4695b2fd3513
shubham agrahari <shubham.agrahari@indexnine.com>
2024-04-28 17:55:57 +0530

    going to build it from scratch

--- youtubePlayer/src/App.tsx
+++ youtubePlayer/src/App.tsx
@@ -1,14 +1,7 @@
-import "./App.css";
-import { PlayerContext } from "./provider/PlayerContext";
-import { initialState } from "./constant/constant";
-import YoutubePlayer from "./componet/YoutubePlayer";
-
 function App() {
   return (
     <>
-      <PlayerContext.Provider value={initialState}>
-        <YoutubePlayer />
-      </PlayerContext.Provider>
+      <>heelo </>
     </>
   );
 }
--- youtubePlayer/src/provider/actions.ts
+++
@@ -1,4 +0,0 @@
-// actions
-export const PLAY_PAUSE = "PLAY_PAUSE";
-export const ON_MUTE = "ON_MUTE";
-export const VOLUME_CHANGE = "VOLUME_CHANGE";
