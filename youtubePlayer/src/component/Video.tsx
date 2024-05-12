1920aa8e4ba58d211b2a8c830be4b36a0ffdee7b
shubham agrahari <shubham.agrahari@indexnine.com>
2024-05-12 15:14:12 +0530

    added video sprites

--- youtubePlayer/src/component/Video.tsx
+++ youtubePlayer/src/component/Video.tsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { PlayerContext, PlayerDispatchContext } from "../context/VideoContext";
 import { PAUSE_PLAY, VIDEO_PROGRESS } from "../context/VideoReducer";
 import VideoToolbar, { StyledViedoToolbar } from "./videoToolbar";
+
 const StyledVideoElement = styled.div``;
 
 const StyledVideoContainer = styled.div`
@@ -13,10 +14,17 @@ const StyledVideoContainer = styled.div`
     visibility: visible; /* Show the toolbar when hovered */
   }
 `;
+const ThumbnailPreview = styled.img`
+  position: absolute;
+  top: -50px; /* Adjust this value as needed */
+  left: 0;
+  display: none;
+`;
 
 const Video = () => {
   const videoElement = useRef<HTMLVideoElement>(null);
   const { isPlaying } = useContext(PlayerContext);
+
   const dispatch = useContext(PlayerDispatchContext);
 
   const onVideoClick = () => {
@@ -51,9 +59,29 @@ const Video = () => {
           ref={videoElement}
           height={"350px"}
           onTimeUpdate={handleOnTimeUpdate}
-        />
+        >
+          <track src={"./videoSprite.vtt"} kind="metadata" />
+        </video>
+        {/* {previewUrl && (
+          <ThumbnailPreview src={previewUrl} alt="Thumbnail Preview" />
+        )} */}
       </StyledVideoElement>
-      <VideoToolbar videoElement={videoElement} onVideoClick={onVideoClick} />
+      {/* {thumbnails.map((thumbnail, index) => (
+        <img
+          key={index}
+          src={thumbnail}
+          alt={`Thumbnail ${index}`}
+          style={{ width: "100px", height: "auto", cursor: "pointer" }}
+          onMouseEnter={() => handleThumbnailHover(index * 10)}
+          onMouseLeave={handleThumbnailOut}
+          onClick={() => handleThumbnailClick(index * 10)} // Assuming interval is 10 seconds
+        />
+      ))} */}
+      <VideoToolbar
+        videoElement={videoElement}
+        onVideoClick={onVideoClick}
+        numFrames={49}
+      />
     </StyledVideoContainer>
   );
 };
--- youtubePlayer/src/component/videoToolbar/index.tsx
+++ youtubePlayer/src/component/videoToolbar/index.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useContext, useState } from "react";
+import { RefObject, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import {
   PlayerContext,
@@ -7,27 +7,61 @@ import {
 import { MUTE_UNMUTE, SPEED, VIDEO_PROGRESS } from "../../context/VideoReducer";
 
 export const StyledViedoToolbar = styled.div`
-  display: flex;
   align-items: center;
   justify-content: space-evenly;
   position: absolute;
   background: red;
   width: 100%;
-  height: 50px;
+  height: 100px;
   bottom: 0rem;
-  background: linear-gradient(to top, black, white);
+  background: linear-gradient(to top, transparent, white);
   color: black;
   visibility: hidden;
   transition: all 0.3s ease-in-out;
+  input[type="range"] {
+    -webkit-appearance: none !important;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 20px;
+    height: 4px;
+    width: 350px;
+  }
+
+  input[type="range"]::-moz-range-progress {
+    background: red;
+  }
 `;
 export type TvideoToolbar = {
   videoElement: RefObject<HTMLVideoElement>;
   onVideoClick: () => void;
+  numFrames: number;
 };
 
-const VideoToolbar = ({ videoElement, onVideoClick }: TvideoToolbar) => {
+const VideoToolbar = ({
+  videoElement,
+  onVideoClick,
+  numFrames,
+}: TvideoToolbar) => {
   const { isPlaying, progress, speed, isMuted } = useContext(PlayerContext);
   const [tooltip, setTooltip] = useState(false);
+  const [currentFrame, setCurrentFrame] = useState<number | null>(null); // State to track the current frame
+  const [percentage, setPercentage] = useState<number | null>(null);
+  const [thumbnails, setThumbnails] = useState<string[]>([]);
+
+  const handleHover = (e) => {
+    if (videoElement.current) {
+      const rect = e.target.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const width = rect.width;
+      const hoverPercentage = (x / width) * 100;
+      console.log("hoverPercentage", hoverPercentage);
+      const currentFrame = Number(
+        (Number(hoverPercentage?.toFixed(0)) / 100) *
+          videoElement.current.duration
+      ).toFixed(0);
+      setPercentage(Number(hoverPercentage));
+      setCurrentFrame(Number(currentFrame));
+    }
+  };
 
   const dispatch = useContext(PlayerDispatchContext);
 
@@ -59,12 +93,37 @@ const VideoToolbar = ({ videoElement, onVideoClick }: TvideoToolbar) => {
     const seconds = Math.floor(time % 60);
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
+
   const handleMouseOver = () => {
     if (videoElement.current) {
       setTooltip(true);
     }
   };
+  const handleLeave = () => {
+    setPercentage(null);
+  };
 
+  useEffect(() => {
+    const loadThumbnails = async () => {
+      const thumbnailPromises = [];
+      for (let i = 1; i <= 47; i++) {
+        const importPromise = import(`./videoSprite/outputSprite_${i}.png`);
+        thumbnailPromises.push(importPromise);
+      }
+
+      const thumbnailModules = await Promise.all(thumbnailPromises);
+      const thumbnailUrls = thumbnailModules.map((module) => module.default);
+      setThumbnails(thumbnailUrls);
+    };
+
+    loadThumbnails();
+  }, []);
+
+  const handleThumbnailClick = (time: number) => {
+    if (videoElement.current) {
+      videoElement.current.currentTime = time;
+    }
+  };
   return (
     <StyledViedoToolbar>
       <div style={{ width: "300px" }} onClick={onVideoClick}>
@@ -73,10 +132,12 @@ const VideoToolbar = ({ videoElement, onVideoClick }: TvideoToolbar) => {
       <div
         style={{
           position: "relative",
-          width: "300px",
         }}
+        onMouseMove={handleHover}
+        onMouseLeave={handleLeave}
       >
         <input
+          style={{}}
           type="range"
           min="0"
           max="100"
@@ -89,6 +150,21 @@ const VideoToolbar = ({ videoElement, onVideoClick }: TvideoToolbar) => {
           <div style={{ position: "absolute", top: "-20px", right: "0rem" }}>
             {formatTime(Number(videoElement?.current.currentTime || 0))}
           </div>
+        )}
+
+        {percentage && thumbnails && (
+          <img
+            src={thumbnails[currentFrame || 0]}
+            style={{
+              width: "100px",
+              height: "auto",
+              cursor: "pointer",
+              position: "absolute",
+              left: `${percentage * 3}px`,
+              bottom: "20px",
+            }}
+            onClick={() => handleThumbnailClick(percentage)} // Assuming interval is 10 seconds
+          />
         )}
       </div>
 
